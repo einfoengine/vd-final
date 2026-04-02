bbf616335ac5f2065142ef83689ffed675f9a8c3
Mohammad Shanewaz Al Maruf <einfoengine@gmail.com>
2026-04-02 16:25:56 +0600

    added untracked files

---
+++ .next/dev/static/chunks/vd-final_src_app_page_tsx_11048758._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/vd-final_src_bf494a87._.js",
+  "static/chunks/5b156_react-icons_fa6_index_mjs_dea31686._.js",
+  "static/chunks/5b156_lucide-react_dist_esm_icons_e1b302bf._.js"
+],
+    source: "dynamic"
+});
